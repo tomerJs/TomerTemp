875835a9fd975578d3a142e7df197da9e4c42401
tomerJs <60569550+tomerJs@users.noreply.github.com>
2020-09-29 12:03:07 +0300

    2 commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>SmokeCheck2.0</Text>
+      <Text>SmokeCheck2.0 !</Text>
       <StatusBar style="auto" />
     </View>
   );
